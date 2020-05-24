0b4ccc0793a476405c4014fb263244d078670a77
tit-Wojciech <64011265+tit-Wojciech@users.noreply.github.com>
2020-05-25 01:02:08 +0200

    Add files via upload

--- palindromy_2/main.cpp
+++ palindromy_2/main.cpp
@@ -14,107 +14,117 @@ fstream o, z1, z2, z3;
 bool sp (string s)
 {
     int dl=s.size();
-    for ( int i=0;i<dl/2;i++)
-    if(s[i]!=s[dl-i-1]) return false;
+    for ( int i=0; i<dl/2; i++)
+        if(s[i]!=s[dl-i-1])
+            return false;
     return true;
 }
 
-
 int main()
 {
-while (true)
-{
-    cout << "================================================================="<<endl;
-    cout << "1-Palindromy zaczynajace sie na pierwsza litere waszego imienia" << endl;
-    cout << "2-Liczba palindromow przystych i nie parzystych"<<endl;
-    cout << "3-Palindromy z litera A wystepujaca conajmniej 3 razy"<<endl;
-    cout << "4-Wylacz program" << endl;
-    cout << "================================================================="<<endl;
-    w=getch();
-    switch (w)
+    while (true)
     {
-    case '1':
-        //--------------Wprowadzanie imienia---------------
-        o.open("dane.txt", ios::in);
-        z1.open("wynik1.txt", ios::out | ios::trunc);
-        cout << "Podaj swoje imie : ";
-        cin >> p;
-        //-------------------------------------------------
-
-        //--------------Warunki i palindromy---------------
-        x=0;
-        while(o.good())
+        cout << "================================================================="<<endl;
+        cout << "1-Palindromy zaczynajace sie na pierwsza litere waszego imienia" << endl;
+        cout << "2-Liczba palindromow przystych i nie parzystych"<<endl;
+        cout << "3-Palindromy z litera A wystepujaca conajmniej 3 razy"<<endl;
+        cout << "4-Wylacz program" << endl;
+        cout << "================================================================="<<endl;
+        w=getch();
+        switch (w)
         {
-            o>>a;
-            if (a[0]==p[0])
+        case '1':
+            //--------------Wprowadzanie imienia---------------
+            o.open("dane.txt", ios::in);
+            z1.open("wynik1.txt", ios::out | ios::trunc);
+            cout << "Podaj swoje imie : ";
+            cin >> p;
+            //-------------------------------------------------
+
+            //--------------Warunki i palindromy---------------
+            x=0;
+            while(o.good())
             {
-                if(sp(a)==true)
+                o>>a;
+                if (a[0]==p[0])
                 {
-                    cout<<a<<endl;
-                    z1<<a<<endl;
-                    x++;
+                    if(sp(a)==true)
+                    {
+                        cout<<a<<endl;
+                        z1<<a<<endl;
+                        x++;
+                    }
+
                 }
 
+
             }
-
-
-        }
-        if (x==0)
+            if (x==0)
             {
                 cout<< "Brak palindromow spelniajacych warunek"<<endl;
                 z1<<"Brak palindromow spelniajacych warunek";
             }
-        //--------------------------------------------------
+            //--------------------------------------------------
 
-        o.close();
-        z1.close();
+            o.close();
+            z1.close();
 
-        break;
+            break;
 
-    case '2':
-        o.open("dane.txt", ios::in);
-        z2.open("wynik2.txt", ios::out);
-        while(o.good())
-        {
-            o>>a;
-            if(sp(a)==true)
+        case '2':
+            o.open("dane.txt", ios::in);
+            z2.open("wynik2.txt", ios::out);
+            while(o.good())
             {
-            x=a.length();
-            if(x%2==0) pa++;
-            else npa++;
+                o>>a;
+                if(sp(a)==true)
+                {
+                    x=a.length();
+                    if(x%2==0)
+                        pa++;
+                    else
+                        npa++;
+                }
             }
-        }
-        cout<<"Parzystych palindromow jest: "<<pa<<endl;
-        cout<<"Nie parzysstych palindromów jest: "<<npa<<endl;
-        z2<<"Parzystych palindromow jest: "<<pa<<endl;
-        z2<<"Nie parzysstych palindromów jest: "<<npa<<endl;
-         o.close();
-        z2.close();
-        break;
+            cout<<"Parzystych palindromow jest: "<<pa<<endl;
+            cout<<"Nie parzysstych palindromów jest: "<<npa<<endl;
+            z2<<"Parzystych palindromow jest: "<<pa<<endl;
+            z2<<"Nie parzysstych palindromów jest: "<<npa<<endl;
+            o.close();
+            z2.close();
+            pa=0;
+            npa=0;
+            break;
 
-    case '3':
-        o.open("dane.txt", ios::in);
-        z3.open("wynik3.txt", ios::out);
-        while(o.good())
-            o>>a;
-            if(sp(a)==true)
-            {
-        if(count(a.begin(),a.end(), 'A')>=3)
+        case '3':
         {
-            cout<<a<<endl;
-            z3<<a<<endl;
+            o.open("dane.txt", ios::in);
+            z3.open("wynik3.txt", ios::out);
+            while(o.good())
+            {
+                o>>a;
+                if(sp(a)==true)
+                {
+                    if(count(a.begin(),a.end(), 'A')>=3)
+                    {
+                        cout<<a<<endl;
+                        z3<<a<<endl;
+                    }
+                }
+            }
+            o.close();
+            z3.close();
         }
-        o.close();
-        z3.close();
-            }
         break;
-    case '4':
-        exit(0);
-        break;
-    default: cout<<"Niepoprawna warotsc"<<endl;
-        break;
-            }
-        getchar();getchar();
+        case '4':
+            exit(0);
+            break;
+        default:
+            cout<<"Niepoprawna warotsc"<<endl;
+            break;
+        }
+        getchar();
+        getchar();
         system("cls");
     }
     return 0;
